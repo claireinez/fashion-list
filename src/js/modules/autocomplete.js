1d8dbab2f6a90d4e6b118eaadd1da01de7b50ff1
Claire Mitchell <claireinez@users.noreply.github.com>
2017-07-20 17:42:35 +0100

    Refactor suggestion chain into separate functions.

--- src/js/modules/autocomplete.js
+++ src/js/modules/autocomplete.js
@@ -1,4 +1,4 @@
-import {getSuggestions} from '../api/suggestions';
+import { getSuggestions } from '../api/suggestions';
 
 
 export default class AutoComplete {
@@ -19,39 +19,44 @@ export default class AutoComplete {
             return this.renderSuggestions(null, []);
         }
 
-        getSuggestions(event.target.value).then(suggestions => {
-            // Only render suggestions for the latest query
-            if (query === this.lastQuery) return this.renderSuggestions(query, suggestions);
-        });
+        getSuggestions(event.target.value)
+            .then(suggestions => {
+                // Only render suggestions for the latest query
+                if (query === this.lastQuery) return this.renderSuggestions(query, suggestions);
+            })
+            .catch(error => {
+                throw new Error(`There was a problem retrieving results for the search term ${ event.target.value }.`);
+            });
 
     }
 
     renderSuggestions(query, suggestions) {
         const list = this.element.querySelector('.autocomplete__suggestions');
 
+        const sortSuggestions
+            = (a, b) => a.toLowerCase().indexOf(query) < b.toLowerCase().indexOf(query)
+                ? -1
+                : 1;
+
+        const wrapWithEmTag = suggestion => suggestion.replace(
+            new RegExp(`(${query})`, 'ig'),
+            '<em>$1</em>'
+        );
+
+        const addSuggestionToList = suggestion => {
+            const listItem = document.createElement('li');
+
+            listItem.className = 'autocomplete__suggestion';
+            listItem.innerHTML = suggestion;
+            list.appendChild(listItem);
+        };
+
         list.innerHTML = '';
 
         suggestions
-            .sort(
-                // Order suggestions by the index of the query in the item
-                (a, b) => a.toLowerCase().indexOf(query) < b.toLowerCase().indexOf(query)
-                    ? -1
-                    : 1
-            )
-            .map(
-                // Wrap the matching part of the suggestion in an <em> tag
-                suggestion => suggestion.replace(
-                    new RegExp(`(${query})`, 'ig'),
-                    '<em>$1</em>'
-                )
-            )
-            .forEach(suggestion => {
-                const listItem = document.createElement('li');
-
-                listItem.className = 'autocomplete__suggestion';
-                listItem.innerHTML = suggestion;
-                list.appendChild(listItem);
-            })
+            .sort(sortSuggestions)
+            .map(wrapWithEmTag)
+            .forEach(addSuggestionToList);
     }
 
 }
